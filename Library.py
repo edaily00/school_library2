28cc645e1f53eaae07a299392d919ce43408bd2c
Eric Daily <dailye@oregonstate.edu>
2022-06-28 14:31:39 -0400

    may

--- Library.py
+++ Library.py
@@ -197,7 +197,7 @@ class Library:
         self._current_date += 1
         for member in self._members:
             for item in self.lookup_patron_from_id(member).get_checked_out_items():
-                if self._current_date >= item.get_date_checked_out() + item.get_check_out_length():
+                if self._current_date > item.get_date_checked_out() + item.get_check_out_length():
                     self.lookup_patron_from_id(member).amend_fine(.10)
 
 
